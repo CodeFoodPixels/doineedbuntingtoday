9b7af221f10380117ade58ee6190cf1cc99bed73
Luke Bonaccorsi <luke.bonaccorsi@and.digital>
2020-05-04 13:13:54 +0100

    Add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
   config.addPassthroughCopy("src/images");
   config.addPassthroughCopy("src/fonts");
+  config.addPassthroughCopy("src/favicon.ico");
   return {
     dir: {
       input: "src",
