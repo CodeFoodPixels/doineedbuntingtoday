c3e9baa15495bd9ab7035191c47e56f8c96e2f16
Luke Bonaccorsi <luke@lukeb.co.uk>
2021-04-13 17:46:29 +0100

    Add more text

--- src/_data/bank_holidays.js
+++ src/_data/bank_holidays.js
@@ -10,21 +10,25 @@ module.exports = async () => {
       name: "england-and-wales",
       displayName: "England",
       bunting: false,
-    },
-    {
-      name: "england-and-wales",
-      displayName: "Wales",
-      bunting: false,
+      bankHoliday: false,
     },
     {
       name: "scotland",
       displayName: "Scotland",
       bunting: false,
+      bankHoliday: false,
+    },
+    {
+      name: "england-and-wales",
+      displayName: "Wales",
+      bunting: false,
+      bankHoliday: false,
     },
     {
       name: "northern-ireland",
       displayName: "Northern Ireland",
       bunting: false,
+      bankHoliday: false,
     },
   ];
 
@@ -33,36 +37,52 @@ module.exports = async () => {
     today.getUTCMonth() + 1
   }`.padStart(2, 0)}-${`${today.getUTCDate()}`.padStart(2, 0)}`;
 
-  const data = structure.map((item) => {
-    const filteredEvents = rawData[item.name].events.filter(
-      (event) => event.date === todayString
-    );
+  const returnObj = {
+    bunting: false,
+    buntingLocations: [],
+    bankHoliday: false,
+    bankHolidayLocations: [],
+  };
 
-    if (filteredEvents.some((event) => event.bunting === true)) {
-      item.bunting = true;
-    }
-    return item;
-  });
+  structure
+    .map((item) => {
+      const filteredEvents = rawData[item.name].events.filter(
+        (event) => event.date === todayString
+      );
 
-  return data
-    .filter((location) => location.bunting === true)
-    .reduce(
-      (obj, location, i, src) => {
-        if (i === 0) {
-          obj.bankHoliday = true;
-          obj.bunting = true;
-          obj.locations = location.displayName;
-          return obj;
-        }
+      if (filteredEvents.length > 0) {
+        item.bankHoliday = true;
+      }
 
-        if (i === src.length - 1) {
-          obj.locations = `${obj.locations} or ${location.displayName}`;
-          return obj;
-        }
+      if (filteredEvents.some((event) => event.bunting === true)) {
+        item.bunting = true;
+      }
+      return item;
+    })
+    .filter((location) => {
+      return location.bankHoliday === true;
+    })
+    .forEach((location, i, src) => {
+      if (location.bunting === true) {
+        returnObj.bunting = true;
+        returnObj.buntingLocations.push(location.displayName);
+      }
 
-        obj.locations = `${obj.locations}, ${location.displayName}`;
-        return obj;
-      },
-      { bunting: false, locations: "", bankHoliday: false }
-    );
+      returnObj.bankHoliday = true;
+      returnObj.bankHolidayLocations.push(location.displayName);
+    });
+
+  returnObj.buntingLocations =
+    returnObj.buntingLocations.length > 1
+      ? `${returnObj.buntingLocations
+          .slice(0, -1)
+          .join(", ")} or ${returnObj.buntingLocations.slice(-1)}`
+      : returnObj.buntingLocations[0];
+  returnObj.bankHolidayLocations =
+    returnObj.bankHolidayLocations.length > 1
+      ? `${returnObj.bankHolidayLocations
+          .slice(0, -1)
+          .join(", ")} and ${returnObj.bankHolidayLocations.slice(-1)}`
+      : returnObj.bankHolidayLocations[0];
+  return returnObj;
 };
